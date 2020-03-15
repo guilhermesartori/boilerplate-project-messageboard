5437440e05b9e0e4eca1ac8169b5c309d9d4ef70
guilhermesartori <guilherme.sartori@grad.ufsc.br>
2020-03-15 14:41:02 -0300

    Tests passing

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,279 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
+const MongoClient = require("mongodb").MongoClient;
+const MONGO_CONNECTION_STRING = process.env.DB;
+const ObjectID = require("mongodb").ObjectID;
 
-module.exports = function (app) {
-  
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+module.exports = function(app) {
+  app
+    .route("/api/threads/:board")
+    // /api/threads/:board
+    .post((req, res) => {
+      const board = req.params.board;
+      const text = req.body.text;
+      const delete_password = req.body.delete_password;
+      if (board && text && delete_password) {
+        const thread = {
+          text: text,
+          created_on: new Date(),
+          bumped_on: new Date(),
+          delete_password: delete_password,
+          reported: false,
+          replies: []
+        };
+        MongoClient.connect(
+          MONGO_CONNECTION_STRING,
+          { useUnifiedTopology: true },
+          (err, con) => {
+            if (err) {
+              return res.send("Database error: " + err);
+            }
+            const db = con.db();
+            db.collection(board).insertOne(thread, (err, data) => {
+              if (err) {
+                return res.send("Database error: " + err);
+              } else {
+                res.redirect(`/b/${board}`);
+              }
+            });
+          }
+        );
+      } else {
+        res.send("Invalid form data.");
+      }
+    })
+    // /api/threads/:board
+    .get((req, res) => {
+      MongoClient.connect(
+        MONGO_CONNECTION_STRING,
+        { useUnifiedTopology: true },
+        (err, con) => {
+          if (err) {
+            return res.send("Database error: " + err);
+          } else {
+            const db = con.db();
+            const board = req.params.board;
+            db.collection(board)
+              .find()
+              .project({
+                reported: 0,
+                delete_password: 0,
+                "replies.delete_password": 0,
+                "replies.reported": 0
+              })
+              .sort({ bumped_on: -1 })
+              .limit(10)
+              .toArray((err, docs) => {
+                if (err) return res.send("Database error: " + err);
+                docs.forEach(doc => {
+                  doc.replycount = doc.replies.length;
+                  if (doc.replies.length > 3) {
+                    doc.replies = doc.replies.slice(-3);
+                  }
+                });
+                res.json(docs);
+              });
+          }
+        }
+      );
+    })
 
+    // /api/threads/:board
+    .delete((req, res) => {
+      const board = req.params.board;
+      const thread_id = req.body.thread_id;
+      const delete_password = req.body.delete_password;
+      MongoClient.connect(
+        MONGO_CONNECTION_STRING,
+        { useUnifiedTopology: true },
+        (err, con) => {
+          if (err) return res.send("Database error: " + err);
+          const db = con.db();
+          db.collection(board).findOneAndDelete(
+            { _id: new ObjectID(thread_id), delete_password: delete_password },
+            (err, doc) => {
+              if (err) return res.send("Database error: " + err);
+              else if (doc.value) res.send("success");
+              else res.send("incorrect password");
+            }
+          );
+        }
+      );
+    })
+    // /api/threads/:board
+    .put((req, res) => {
+      const board = req.params.board;
+      const thread_id = req.body.report_id;
+      MongoClient.connect(
+        MONGO_CONNECTION_STRING,
+        { useUnifiedTopology: true },
+        (err, con) => {
+          const db = con.db();
+          db.collection(board).findOneAndUpdate(
+            { _id: new ObjectID(thread_id) },
+            { $set: { reported: true } },
+            (err, doc) => {
+              if (err) return res.send("Database error: " + err);
+              res.send("success");
+            }
+          );
+        }
+      );
+    });
+
+  app
+    .route("/api/replies/:board")
+    // /api/replies/:board
+    .post((req, res) => {
+      const board = req.params.board;
+      const text = req.body.text;
+      const delete_password = req.body.delete_password;
+      const thread_id = req.body.thread_id;
+      if (board && text && delete_password && thread_id) {
+        const reply = {
+          _id: new ObjectID(),
+          text: text,
+          created_on: new Date(),
+          delete_password: delete_password,
+          reported: false
+        };
+        MongoClient.connect(
+          MONGO_CONNECTION_STRING,
+          { useUnifiedTopology: true },
+          (err, con) => {
+            if (err) {
+              return res.send("Database error: " + err);
+            }
+            const db = con.db();
+            db.collection(board).findOneAndUpdate(
+              { _id: new ObjectID(thread_id) },
+              {
+                $push: {
+                  replies: { $each: [reply], $sort: { created_on: 1 } }
+                },
+                $set: { bumped_on: new Date() }
+              },
+              { returnNewDocument: true },
+              (err, data) => {
+                if (err) {
+                  return res.send("Database error: " + err);
+                } else {
+                  res.redirect(`/b/${board}/${thread_id}`);
+                }
+              }
+            );
+          }
+        );
+      } else {
+        res.send("Invalid form data.");
+      }
+    })
+    // /api/replies/:board
+    .get((req, res) => {
+      MongoClient.connect(
+        MONGO_CONNECTION_STRING,
+        { useUnifiedTopology: true },
+        (err, con) => {
+          if (err) {
+            return res.send("Database error: " + err);
+          } else {
+            const db = con.db();
+            const board = req.params.board;
+            const thread_id = req.query.thread_id;
+            db.collection(board)
+              .find({ _id: new ObjectID(thread_id) })
+              .project({
+                reported: 0,
+                delete_password: 0,
+                "replies.delete_password": 0,
+                "replies.reported": 0
+              })
+              .toArray((err, docs) => {
+                if (err) return res.send("Database error: " + err);
+                else res.json(docs[0]);
+              });
+          }
+        }
+      );
+    })
+    // /api/replies/:board
+    .delete((req, res) => {
+      MongoClient.connect(
+        MONGO_CONNECTION_STRING,
+        { useUnifiedTopology: true },
+        (err, con) => {
+          if (err) {
+            return res.send("Database error: " + err);
+          } else {
+            const db = con.db();
+            const board = req.params.board;
+            const thread_id = req.body.thread_id;
+            const reply_id = req.body.reply_id;
+            const delete_password = req.body.reply_id;
+
+            db.collection(board).findOneAndUpdate(
+              {
+                _id: new ObjectID(thread_id),
+                replies: {
+                  $elemMatch: {
+                    _id: new ObjectID(req.body.reply_id),
+                    delete_password: req.body.delete_password
+                  }
+                }
+              },
+              { $set: { "replies.$.text": "[deleted]" } },
+              { returnNewDocument: true },
+              (err, doc) => {
+                if (err) return res.send("Database error: " + err);
+                else if (doc.value) return res.send("success");
+                else return res.send("incorrect password");
+              }
+            );
+          }
+        }
+      );
+    })
+
+    // /api/replies/:board
+    .put((req, res) => {
+      MongoClient.connect(
+        MONGO_CONNECTION_STRING,
+        { useUnifiedTopology: true },
+        (err, con) => {
+          if (err) {
+            return res.send("Database error: " + err);
+          } else {
+            const db = con.db();
+            const board = req.params.board;
+            const thread_id = req.body.thread_id;
+            const reply_id = req.body.reply_id;
+
+            db.collection(board).findOneAndUpdate(
+              {
+                _id: new ObjectID(thread_id),
+                replies: {
+                  $elemMatch: {
+                    _id: new ObjectID(req.body.reply_id)
+                  }
+                }
+              },
+              { $set: { "replies.$.reported": true } },
+              { returnNewDocument: true },
+              (err, doc) => {
+                if (err) return res.send("Database error: " + err);
+                res.send("success");
+              }
+            );
+          }
+        }
+      );
+    });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,59 +1,201 @@
 /*
-*
-*
-*       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
-*       -----[Keep the tests in the same order!]-----
-*       (if additional are added, keep them at the very end!)
-*/
+ *
+ *
+ *       FILL IN EACH FUNCTIONAL TEST BELOW COMPLETELY
+ *       -----[Keep the tests in the same order!]-----
+ *       (if additional are added, keep them at the very end!)
+ */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
+var chaiHttp = require("chai-http");
+var chai = require("chai");
 var assert = chai.assert;
-var server = require('../server');
+var server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite("Functional Tests", function() {
+  let testText = Math.floor(Math.random() * 10000000);
+  let testId;
+  let testId2;
+  let testId3;
 
-  suite('API ROUTING FOR /api/threads/:board', function() {
-    
-    suite('POST', function() {
-      
+  suite("API ROUTING FOR /api/threads/:board", function() {
+    suite("POST", function(done) {
+      test("create 2 new threads(because we end up deleting 1 in the delete test)", function(done) {
+        chai
+          .request(server)
+          .post("/api/threads/test")
+          .send({ text: "Test thread", delete_password: "123" })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+          });
+        chai
+          .request(server)
+          .post("/api/threads/test")
+          .send({ text: "Test thread 2", delete_password: "123" })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
     });
-    
-    suite('GET', function() {
-      
-    });
-    
-    suite('DELETE', function() {
-      
-    });
-    
-    suite('PUT', function() {
-      
-    });
-    
 
+    suite("GET", function(done) {
+      test("most recent 10 threads with most recent 3 replies each", function(done) {
+        chai
+          .request(server)
+          .get("/api/threads/test")
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.isArray(res.body);
+            assert.property(res.body[0], "_id");
+            assert.property(res.body[0], "text");
+            assert.property(res.body[0], "replies");
+            assert.property(res.body[0], "created_on");
+            assert.property(res.body[0], "bumped_on");
+            assert.isArray(res.body[0].replies);
+            assert.isBelow(res.body[0].replies.length, 4);
+            assert.notProperty(res.body[0], "reported");
+            assert.notProperty(res.body[0], "delete_password");
+            testId = res.body[0]._id;
+            testId2 = res.body[1]._id;
+            done();
+          });
+      });
+    });
+
+    suite("DELETE", function(done) {
+      test("delete thread with good password", function(done) {
+        chai
+          .request(server)
+          .delete("/api/threads/test")
+          .send({ thread_id: testId, delete_password: "123" })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, "success");
+            done();
+          });
+      });
+
+      test("delete thread with bad password", function(done) {
+        chai
+          .request(server)
+          .delete("/api/threads/test")
+          .send({ thread_id: testId2, delete_password: "321" })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, "incorrect password");
+            done();
+          });
+      });
+    });
+
+    suite("PUT", function() {
+      test("report thread", function(done) {
+        chai
+          .request(server)
+          .put("/api/threads/test")
+          .send({ thread_id: testId2 })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, "success");
+            done();
+          });
+      });
+    });
   });
-  
-  suite('API ROUTING FOR /api/replies/:board', function() {
-    
-    suite('POST', function() {
-      
-    });
-    
-    suite('GET', function() {
-      
-    });
-    
-    suite('PUT', function() {
-      
-    });
-    
-    suite('DELETE', function() {
-      
-    });
-    
-  });
 
+  suite("API ROUTING FOR /api/replies/:board", function() {
+    suite("POST", function() {
+      test("reply to thread", function(done) {
+        chai
+          .request(server)
+          .post("/api/replies/test")
+          .send({
+            thread_id: testId2,
+            text: "My number is " + testText,
+            delete_password: "123"
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            done();
+          });
+      });
+    });
+
+    suite("GET", function() {
+      test("Get all replies for 1 thread", function(done) {
+        chai
+          .request(server)
+          .get(`/api/replies/test?thread_id=${testId2}`)
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            console.log(res.body)
+            assert.property(res.body, "_id");
+            assert.property(res.body, "created_on");
+            assert.property(res.body, "bumped_on");
+            assert.property(res.body, "text");
+            assert.property(res.body, "replies");
+            assert.notProperty(res.body, "delete_password");
+            assert.notProperty(res.body, "reported");
+            assert.isArray(res.body.replies);
+            assert.notProperty(res.body.replies[0], "delete_password");
+            assert.notProperty(res.body.replies[0], "reported");
+            assert.equal(
+              res.body.replies[res.body.replies.length - 1].text,
+              "My number is " + testText
+            );
+            done();
+          });
+      });
+    });
+
+    suite("PUT", function() {
+      test("report reply", function(done) {
+        chai
+          .request(server)
+          .put("/api/threads/test")
+          .send({ thread_id: testId2, reply_id: testId2 })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, "success");
+            done();
+          });
+      });
+    });
+
+    suite("DELETE", function() {
+      test("delete reply with bad password", function(done) {
+        chai
+          .request(server)
+          .delete("/api/threads/test")
+          .send({
+            thread_id: testId2,
+            reply_id: testId3,
+            delete_password: "321"
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, "incorrect password");
+            done();
+          });
+      });
+
+      test("delete reply with valid password", function(done) {
+        chai
+          .request(server)
+          .delete("/api/threads/test")
+          .send({
+            thread_id: testId2,
+            reply_id: testId3,
+            delete_password: "123"
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, "success");
+            done();
+          });
+      });
+    });
+  });
 });
